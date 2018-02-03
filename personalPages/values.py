ac7534741de42adfd1796cc3d3bc7d7f2a616d8a
Ros <liandtow@gmail.com>
2018-02-03 05:43:29 +0000

    improved footer, placeholder text

--- personalPages/values.py
+++ personalPages/values.py
@@ -1,8 +1,22 @@
 
 FULL_NAME = "John Doe"
-PROFESSIONAL_TITLE = "Your Profession Here" #You can leave this empty to hide your profession
-EMAIL = "johndoe@placeholder.johndoe"
-RESUME_IMAGE_URL = "http://dummyimage.com/600x1000&text=resume_image_placeholder"
+PROFESSIONAL_TITLE = "Carpenter" #You can leave this empty to hide your profession subtitle
 
+# WebPage Footer links
+# If you don't want to show any simply leav an empty string
+GITHUB = "https://github.com/"
+LINKEDIN = "https://www.linkedin.com/"
+EMAIL = "johndoe@placeholder.johndoe"
+
+BASIC_INFO = {
+    'fullname': FULL_NAME,
+    'professional_title': PROFESSIONAL_TITLE,
+    'github': GITHUB,
+    'linkedin': LINKEDIN,
+    'email': EMAIL,
+}
+
+# RESUME
+RESUME_IMAGE_URL = "http://dummyimage.com/600x1000&text=resume_image_placeholder"
 RESUME_PDF_URL = "https://cpd.org.au/wp-content/uploads/2014/11/placeholder.pdf"
 
--- personalPages/views.py
+++ personalPages/views.py
@@ -9,17 +9,14 @@ def index(request):
 
 def resume(request):
     context = {
-        'fullname': values.FULL_NAME,
         'resume_pdf_url': values.RESUME_PDF_URL,
         'resume_image_url': values.RESUME_IMAGE_URL,
-        'email': values.EMAIL,
-        'professional_title': values.PROFESSIONAL_TITLE,
     }
+    context.update(values.BASIC_INFO)
     return render(request, 'personalPages/resume.html', context)
 
 def about(request):
     context = {
-        'fullname': values.FULL_NAME,
-        'professional_title': values.PROFESSIONAL_TITLE,
     }
+    context.update(values.BASIC_INFO)
     return render(request, 'personalPages/about.html', context)
