a0e366c059354a1f1db673e0e0cf1891986fec5e
Rostyslav Khoptiy <ros65536@gmail.com>
2018-06-25 12:47:18 +0100

    refactor css into sass, fix footer to bottom

--- personalPages/compile_assets.py
+++ personalPages/compile_assets.py
@@ -1,7 +1,8 @@
 import sass 
 import os
 
-# sass
 SASS_DIR='css/sass'
 CSS_OUTPUT_DIR='static/personalPages/css/'
+
+# sass
 sass.compile(dirname=(SASS_DIR, CSS_OUTPUT_DIR), output_style='compressed')
