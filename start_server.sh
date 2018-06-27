884db1892d6c5c2e8f9691d169ef383e3a4bda83
Rostyslav Khoptiy <ros65536@gmail.com>
2018-06-27 23:54:06 +0100

    improve script prints

--- start_server.sh
+++ start_server.sh
@@ -9,11 +9,13 @@ if [ -n "$ADMIN_PASS" ];
 	then
     # Setup admin account in database    
     echo -e "from django.contrib.auth.models import User\n\nif(not User.objects.filter(username='admin').all()):\n\tUser.objects.create_superuser('admin', '$ADMIN_EMAIL', '$ADMIN_PASS')\nelse:\n\tprint('admin account already exists, no account created')" | python3 manage.py shell && \
-    echo -e "${ACCENT}Web app admin account possibly created ${NV}with username 'admin', login at <hostname>/admin"
+    echo -e "Web app ${ACCENT}admin account POSSIBLY created ${NC}with username 'admin', login at <hostname>/admin"
 
     unset ADMIN_EMAIL
     unset ADMIN_PASS
 fi
+
+echo -e "Website using Person's table tuple with username ${ACCENT}${P_USERNAME}${NC}"
 
 # Start Gunicorn processes
 echo Starting Gunicorn on port 8000.
