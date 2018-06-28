b4071f4c0c104c3623f37e6258633fa31897f706
Rostyslav Khoptiy <ros65536@gmail.com>
2018-06-28 23:04:39 +0100

    improve VPS  deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,11 @@
 if [ -z "$1" ]
-    then
-    echo "Specify destination user and machine as command argument: <user>:<machine>"
+    then 
+    echo "Specify a .env file location"
     exit 1
 fi
 
-ssh $1 'mkdir -p /code/nginx'
-ssh $1 'mkdir -p /code/uploads'
-scp nginx/PersonalWebsite.conf $1:/code/nginx/PersonalWebsite.conf
-scp docker-compose.yml $1:/code/docker-compose.yml
-scp dotenv.sh $1:/code/dotenv.sh
-scp default.env $1:/code/default.env
-
-# unset ADMIN_EMAIL
-#     unset ADMIN_PASS
+source dotenv.sh $1
+docker pull $DOCKER_TAG_NAME
+docker-compose stop && docker-compose up -d
+unset ADMIN_PASS
+unset ADMIN_EMAIL
